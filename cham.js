700faa7094d76cc01277f896f8b51bf20bcec4a1
ABUZAKUK <sidonie.abuzakuk@grenoble-inp.org>
2025-07-02 21:12:02 +0200

    Add Chrome install for Render

--- cham.js
+++ cham.js
@@ -15,7 +15,10 @@ app.get('/chamilo', async (req, res) => {
   try {
     browser = await puppeteer.launch({
       headless: true,
+
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
+      console.log('🧭 Chrome path:', process.env.PUPPETEER_EXECUTABLE_PATH);
+
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
